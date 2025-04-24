074eef138839cf6f47224bf4c61636ba14368ca1
lakkiy <liubolovelife@gmail.com>
2025-04-24 14:31:18 +0800

    fix eat term

--- lisp/init-shell.el
+++ lisp/init-shell.el
@@ -9,14 +9,8 @@
 ;; https://abode.karthinks.com/share/eat-modes.png
 (install-package 'eat)
 
-(defun jps-eat-term-name (&optional display)
-  "Set terminal to xterm on remote hosts to ensure that backspace works.
-https://codeberg.org/akib/emacs-eat/issues/119 "
-  (if (file-remote-p default-directory)
-      "xterm-256color"
-    (eat-term-get-suitable-term-name display)))
-
-(setq eat-term-name 'jps-eat-term-name)
+;; https://codeberg.org/akib/emacs-eat/issues/119
+(setq eat-term-name "xterm-256color")
 (setq eat-kill-buffer-on-exit t
       eat-enable-directory-tracking t)
 
